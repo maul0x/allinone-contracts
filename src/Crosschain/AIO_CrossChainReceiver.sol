1ca3f1028dfff738ae3477a95bd5ba8e3cbd4a36
maul0x <501staudits@gmail.com>
2025-10-03 09:42:44 -0700

    feat: forge fmt

--- src/Crosschain/AIO_CrossChainReceiver.sol
+++ src/Crosschain/AIO_CrossChainReceiver.sol
@@ -10,8 +10,8 @@ import "lib/wormhole-solidity-sdk/src/interfaces/IERC20.sol";
  * @notice A contract to handle receiving of tokens cross chain using wormhole cross chain token transfer.
  * @notice This contract is deployed on multiple chains to support multiple target chains.
  */
-contract AIO_CrossChainReceiver is TokenReceiver { 
-        // The wormhole relayer and registeredSenders are inherited from the Base.sol contract.
+contract AIO_CrossChainReceiver is TokenReceiver {
+    // The wormhole relayer and registeredSenders are inherited from the Base.sol contract.
 
     constructor(
         address _wormholeRelayer,
