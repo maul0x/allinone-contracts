0f2f5a86cefd59050c50dd00c5eac2d5a635bd09
maul0x <501staudits@gmail.com>
2025-10-04 12:44:52 -0700

    feat: forge fmt

--- src/Crosschain/AIO_CrossChainSender.sol
+++ src/Crosschain/AIO_CrossChainSender.sol
@@ -6,8 +6,8 @@ import "lib/wormhole-solidity-sdk/src/interfaces/IERC20.sol";
 
 contract CrossChainSender is TokenSender {
     uint256 constant GAS_LIMIT = 250_000;
-    
-        constructor(
+
+    constructor(
         address _wormholeRelayer,
         address _tokenBridge,
         address _wormhole
